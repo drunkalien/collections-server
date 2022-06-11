204a53d8363bf4e7f7b8af32765fcdb3e9c92eed
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-06-11 12:45:42 +0500

    created configurations file

---
+++ src/config/config.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+type Config = {
+  HttpPort: string;
+  MongoPort: string;
+  MongoDatabase: string;
+  JwtSecret: string;
+};
+
+const config: Config = {
+  HttpPort: getConf("PORT", "5000"),
+  MongoPort: getConf("MONGO_PORT", "27217"),
+  MongoDatabase: getConf("MONGO_DATABASE", "Task7"),
+  JwtSecret: getConf("JWT_SECRET", "secret"),
+};
+
+function getConf(name: string, def: string): string {
+  if (process.env[name]) {
+    process.env[name] || "";
+  }
+
+  return def;
+}
+
+export default config;
