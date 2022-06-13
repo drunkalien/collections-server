1ba82b04b6aaa051687daf8dd4afe4825b286d8e
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-06-13 19:08:17 +0500

    imported item and comment service

--- src/storage/main.ts
+++ src/storage/main.ts
@@ -1,12 +1,18 @@
 import { UserService } from "./services/user";
 import { CollectionService } from "./services/collection";
+import { CommentService } from "./services/comment";
+import { ItemService } from "./services/item";
 
 type ServiceType = {
   user: UserService;
   collection: CollectionService;
+  comment: CommentService;
+  item: ItemService;
 };
 
 export const service: ServiceType = {
   user: new UserService(),
   collection: new CollectionService(),
+  comment: new CommentService(),
+  item: new ItemService(),
 };
