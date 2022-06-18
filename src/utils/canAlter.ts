7c01586902cfbf0fb70a2f1199e3622b2f6cad73
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-06-18 22:36:25 +0500

    updated canAlter function

--- src/utils/canAlter.ts
+++ src/utils/canAlter.ts
@@ -1,8 +1,5 @@
 import { ICollection } from "../models/Collection";
 
 export function canAlter(user: string, collection: ICollection): boolean {
-  if (user !== collection.author.toString()) {
-    return false;
-  }
-  return true;
+  return user === collection.author.toString();
 }
