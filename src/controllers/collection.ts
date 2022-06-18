6c37b0d3b086529485edbeafaa17f03fd4102d42
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-06-18 20:45:46 +0500

    deleted unnecessary import

--- src/controllers/collection.ts
+++ src/controllers/collection.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { service } from "../storage/main";
 
 export class CollectionController {
