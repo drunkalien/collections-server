32b808ebb9bd7f070c0015c73bc812988fd8ca62
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-06-23 17:06:41 +0500

    added body parser

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,28 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import collectionRouter from "./routes/collection";
 import itemRouter from "./routes/item";
 import userRouter from "./routes/user";
 
+const cloudinary = require("cloudinary");
+
 const app = express();
 const BASE = "/api/v1";
 
+cloudinary.config({
+  cloud_name: "dbpb9xu8f",
+  api_key: "474424751979154",
+  api_secret: "FlE3Q9SYC0YNko5izRrwNwM7BD4",
+});
+
 app.use(
   cors({
-    origin: "http://loclahost:3000",
+    origin: "http://localhost:3000",
   })
 );
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(`${BASE}/collections`, collectionRouter);
 app.use(`${BASE}/items`, itemRouter);
